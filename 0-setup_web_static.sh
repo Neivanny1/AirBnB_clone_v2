beb86271e034c1157f9bd1ff2fd608dcb58fd53e
Neivanny1 <nevileneivanny@gmail.com>
2023-12-07 09:34:06 -0800

    server fixed

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,18 +1,74 @@
 #!/usr/bin/env bash
-# prepares nginx server for static deployment
-sudo apt update
-sudo apt install nginx -y
+# Installs and prepares nginx server for static deployment
 
-# make required folders
-sudo mkdir -p /data/web_static/releases/test /data/web_static/shared/
-echo "<h1>Welcome to tech coders</h1>" | sudo dd status=none of=/data/web_static/releases/test/index.html
-# creates symbolic link
-sudo ln -sf /data/web_static/releases/test /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data/
 
-#settting backup for config files
-sudo cp /etc/nginx/sites-enabled/default nginx-sites-enabled_default.backup
-# setting contents for redirect
-sudo sed -i '37i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
-# restarts the server
-sudo service nginx restart
+SERVER_CONFIG="server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+
+    server_name _;
+    index index.html index.htm;
+    error_page 404 /404.html;
+    add_header X-Served-By \$hostname;
+
+    location / {
+        root /var/www/html/;
+        try_files \$uri \$uri/ =404;
+    }
+
+    location /hbnb_static/ {
+        alias /data/web_static/current/;
+        try_files \$uri \$uri/ =404;
+    }
+
+    if (\$request_filename ~ redirect_me) {
+        rewrite ^ https://sketchfab.com/bluepeno/models permanent;
+    }
+
+    location = /404.html {
+        root /var/www/error/;
+        internal;
+    }
+}"
+
+HOME_PAGE="<!DOCTYPE html>
+<html lang='en-US'>
+    <head>
+        <title>Home - Mombasa Tech Hub</title>
+    </head>
+    <body>
+        <h1>Welcome To Tech Hub Mombasa</h1>
+    </body>
+</html>
+"
+
+# Check if Nginx is installed
+if ! command -v nginx &> /dev/null; then
+    apt-get update && apt-get -y install nginx
+fi
+
+# Create necessary directories
+mkdir -p /var/www/html /var/www/error /data/web_static/releases/test /data/web_static/shared
+
+# Set permissions
+chmod -R 755 /var/www
+chown -R ubuntu:ubuntu /data
+
+# Create fake HTML file
+echo -e "$HOME_PAGE" > /data/web_static/releases/test/index.html
+
+# Create or recreate symbolic link
+[ -e /data/web_static/current ] && rm -rf /data/web_static/current
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+# Update Nginx configuration
+bash -c "echo -e '$SERVER_CONFIG' > /etc/nginx/sites-available/default"
+ln -sf '/etc/nginx/sites-available/default' '/etc/nginx/sites-enabled/default'
+
+# Restart Nginx
+if [ "$(pgrep -c nginx)" -le 0 ]; then
+    service nginx start
+else
+    service nginx restart
+fi
+
