e43e05e53bec785ea24b4a957a0c4ff4189308e5
Neivanny1 <nevileneivanny@gmail.com>
2024-01-01 18:24:56 +0000

    all states in db on html

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Starts a Flask web application
+"""
+
+
+import os
+from models import storage
+from models.state import State
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown(self):
+    """ removes current SQLAlchemy Session """
+    storage.close()
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """Display a HTML page with States"""
+    states = storage.all(State).values()
+    return render_template('8-cities_by_states.html', states=states)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
